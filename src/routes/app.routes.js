41cd74dfb6b56956775000a6abec6bcfe08e078e
Guido Stocco <guidostoccodev@gmail.com>
2025-07-26 18:39:21 -0300

    criando componentes de navegação

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,17 +1,25 @@
 import { View, Text } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../pages/SignIn';
-
+import ChatRoom from '../pages/ChatRoom'
 const Stack = createNativeStackNavigator();
 
 export default function AppRoutes() {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName='ChatRoom'>
             <Stack.Screen
                 name='SignIn'
                 component={SignIn}
                 options={{
                     title: 'Faça o login'
+                }}
+            />
+
+            <Stack.Screen
+                name='ChatRoom'
+                component={ChatRoom}
+                options={{
+                    headerShown: false
                 }}
             />
         </Stack.Navigator>
