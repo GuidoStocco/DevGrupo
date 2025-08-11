b5f5dffa0ade1111d9f30aa9ca3e5f13d8ea9a61
Guido Stocco <guidostoccodev@gmail.com>
2025-08-11 20:56:29 -0300

    criando tela chatroom

--- src/pages/ChatRoom/index.js
+++ src/pages/ChatRoom/index.js
@@ -1,16 +1,68 @@
 import React from 'react';
-import {Text, View, Button } from 'react-native';
+import {Text, View, Button, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import { Feather } from '@expo/vector-icons';
+import auth from "@react-native-firebase/auth";
 
 export default function ChatRoom() {
   const navigation = useNavigation();
+
+  const handleSignOut = () => {
+    auth()
+    .signOut()
+    .then(() => {
+      navigation.navigate("SignIn")
+    })
+    .catch(() => {
+      console.error("Sign out failed");
+    })
+  }
+
   
   return (
-    <View>
-      <Text>Tela ChatRoom</Text>
-      <Button title='login' onPress={() => navigation.navigate('SignIn')}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerRoom}>
+        <View style={styles.headerArrow}>
+          <TouchableOpacity onPress={handleSignOut}>
+            <Feather name="arrow-left" size={28} color="#fff"/>
+          </TouchableOpacity>
+
+          <Text style={styles.headerTitle}>Grupos</Text>
+        </View>
+
+        <TouchableOpacity style={styles.headerSearch}>
+          <Feather name="search" size={28} color="#fff" />
+        </TouchableOpacity>
+        
+      </View>
+    </SafeAreaView>
   );
 }
 
+
+
+const styles = {
+  container: {
+    flex: 1,
+  },
+  headerRoom:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 80,
+    paddingBottom: 20,
+    backgroundColor: '#2e54d4',
+    paddingHorizontal: 10,
+    borderBottomLeftRadius:20,
+    borderBottomRightRadius:20,
+  },
+  headerArrow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    color: '#fff',
+    fontSize: 26,
+    marginLeft: 10,
+  },
+};
